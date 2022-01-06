94e0892f614731e303b5c10cd49bff4ee26908b0
Rob174 <robin.128.456@gmail.com>
2022-01-05 22:29:32 -0400

    first commit

---
+++ commiter/src/enums.py
@@ -0,0 +1,11 @@
+from enums import Enum
+
+class StatusEnum(Enum):
+    DONE = "done"
+    BUILDING = "building" 
+    IN_PROGRESS = "in_progress"
+    RUNNING = "running"
+        
+    
+    
+("!done", "✅"), ("!building","🔨"), ("iprog","⏲️"),("!wait","⏳️"),("!pb","🚩"),("!bug","🐛"),("!goodenough","〰️"),("!pause","🛑"),("!release","🛰️")]
